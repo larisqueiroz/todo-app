8f3b57ecba0ab25d94b1a7dcbc3653a2aa7bb5a3
Larissa Queiroz <larissa.queiroz95@gmail.com>
2024-04-28 18:46:16 -0300

    edit settings

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -134,7 +134,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',)
+    #'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',)
 }
 
 #CORS_ALLOW_CREDENTIALS = True
